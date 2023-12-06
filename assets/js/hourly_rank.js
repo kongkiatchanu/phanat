d002af76f04deaafdd99767e898f5549afc48713
signoutz <aunkaffpom@gmail.com>
2023-12-06 11:45:36 +0700

    asd

--- assets/js/hourly_rank.js
+++ assets/js/hourly_rank.js
@@ -55,7 +55,6 @@ $(function () {
         }
         $.getJSON(data_url, function (db) {
             if (db) {
-                console.log(db);
                 var table = $('#table_pm25_nearby').DataTable();
                 var lang = Cookies.get("lang_cookie");
                 if (lang == 'EN') {
